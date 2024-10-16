2c60027008648717919a4e904027b52d8189383c
Kyle Crosby <acrosby2@nd.edu>
2024-10-16 17:37:22 -0400

    Update makePlot_teamkyle.py

--- makePlot_teamkyle.py
+++ makePlot_teamkyle.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Load the CSV file
-file_path = 'output/test_data_new2.csv'
+file_path = 'output/output_teamkyle.csv'
 data = pd.read_csv(file_path)
 
 # Prepare the data for plotting
